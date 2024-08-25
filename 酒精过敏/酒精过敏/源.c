aa9edaeb51a71e9cef9ec75c5350eb23225e70c6
switch <157223393+justice049@users.noreply.github.com>
2024-08-25 23:24:02 +0800

    Add files via upload

---
+++ 酒精过敏/酒精过敏/源.c
@@ -0,0 +1,42 @@
+﻿#include<stdio.h>
+int divide(int a, int b)
+{
+    int count = 0;
+    if (b > 0 && a > 0 || a < 0 && b < 0)
+    {
+        while (a > b)
+        {
+            a = a - b;
+            count++;
+        }
+        return count;
+    }
+    if (b < 0)
+    {
+        b = -b;
+        while (a > b)
+        {
+            a = a - b;
+            count++;
+        }
+        count = -count;
+        return count;
+    }
+    if (a < 0)
+    {
+        a = -a;
+        while (a > b)
+        {
+            a = a - b;
+            count++;
+        }
+        count = -count;
+        return count;
+    }
+}
+int main()
+{
+    int a = divide(-7, 3);
+    printf("%d", a);
+    return 0;
+}
